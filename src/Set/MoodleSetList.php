0d8c8183e04026003cd8f11bb11f32fdf5d6f6f4
Michael Aherne <michael.aherne@strath.ac.uk>
2024-01-30 16:16:40 +0000

    Added MoodleSetList class.

---
+++ src/Set/MoodleSetList.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorMoodle\Set;
+
+final class MoodleSetList
+{
+
+    final public const MOODLE_42 = __DIR__ . "/../../config/sets/moodle42.php";
+
+}
