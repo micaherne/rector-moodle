a292372897c1d88a9d7b6c0f515610e48bcbd127
Michael Aherne <michael.aherne@strath.ac.uk>
2024-01-31 14:43:06 +0000

    Change require_once to require

--- config/sets/moodle43.php
+++ config/sets/moodle43.php
@@ -6,8 +6,8 @@ use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
 
-    $moodle43renames = require_once __DIR__ . '/../extracted/renamed-classes-v4.3.0.php';
-    $moodle42renames = require_once __DIR__ . '/../extracted/renamed-classes-v4.2.0.php';
+    $moodle43renames = require __DIR__ . '/../extracted/renamed-classes-v4.3.0.php';
+    $moodle42renames = require __DIR__ . '/../extracted/renamed-classes-v4.2.0.php';
 
     $newrenames = array_diff_key($moodle43renames, $moodle42renames);
 
